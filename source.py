df4284d95bb0d977c05551a57017623e8699eeb4
Abdolrahman Farshgar <ifarshgar@gmail.com>
2017-10-24 22:09:17 +0330

    minor changes

--- source.py
+++ source.py
@@ -8,3 +8,17 @@ hello = 'hello';
 output = one.__str__() + two.__str__() + hello
 
 print(output)
+
+
+mylist = [1, 2]
+print(mylist[0])
+print(mylist[1])
+mylist.append(3)
+print(mylist[2])
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
+
+
+#some minor changes
