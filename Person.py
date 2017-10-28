39c77ac44daf76329a3aeed573508d5ee6ee77b6
Abdolrahman Farshgar <ifarshgar@gmail.com>
2017-10-28 16:13:24 +0330

    October 28, 2017 Starting...

---
+++ Person.py
@@ -0,0 +1,13 @@
+
+firstName = "Abdolrahman"
+lastName = "Farshgar"
+age = 23
+major = "Software Engineer"
+
+
+dataArray = (firstName, lastName, age, major)
+formatString = "I'm %s %s. I'm %s. My major is %s"
+
+print(formatString % dataArray)
+
+
