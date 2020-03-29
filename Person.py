e3b9491d2d1fe669a0ceff76b775c63093ca05c7
Abdolrahman <ifarshgar@gmail.com>
2020-03-29 18:53:42 +0300

    extra line removed

--- Person.py
+++ Person.py
@@ -1,9 +1,7 @@
-
 firstName = "Abdolrahman"
 lastName = "Farshgar"
 age = 23
 major = "Software Engineering"
-
 
 dataArray = (firstName, lastName, age, major)
 formatString = "I'm %s %s. I'm %s. My major is %s"
